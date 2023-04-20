59db180adf9a38f48378ff498700fa88e41fdbda
rahul <rahuld030206@gmail.com>
2023-04-20 17:25:39 +0530

    stock news fetched and displayed on page responsive remaining

--- src/Component/StockNews/Stocknews.jsx
+++ src/Component/StockNews/Stocknews.jsx
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { useEffect } from "react";
 
 const Stocknews = ({ stock_name }) => {
+  const [news, setnews] = useState();
+  if (stock_name === undefined) {
+    stock_name = "AAPL";
+  }
   useEffect(() => {
     const fetchData = async () => {
-      
-
-      const stock_news_url = `https://api.polygon.io/v2/reference/news?ticker=AAPL&limit=5&apiKey=B1Q2GnpC7LkFbaouZR3YxNgrtTcgGalG`;
+      const stock_news_url = `https://api.polygon.io/v2/reference/news?ticker=${stock_name}&limit=5&apiKey=B1Q2GnpC7LkFbaouZR3YxNgrtTcgGalG`;
       const stock_news_response = await fetch(stock_news_url);
       const stock_news = await stock_news_response.json();
+      setnews(stock_news.results);
       console.log(stock_news);
     };
     fetchData();
@@ -16,25 +19,33 @@ const Stocknews = ({ stock_name }) => {
   return (
     <div className="stock_news_container">
       <div className="news_head">
-        <h1 className="text-xl font-bold leading-none text-black dark:text-white">
-          {stock_name}-NEWS
+        <h1 className="text-xl font-bold leading-none text-black dark:text-white self-center	text-center	">
+          Trending NEWS
         </h1>
       </div>
-      <div className="news_card  w-[17rem]  bg-white border border-gray-200 rounded-lg shadow">
-        <div className="news_title">
-          Apple to Spend Billions More on Entertainment
-        </div>
-        <div className="news_details">
-          <div className="news_desc">
-            <h4>
-              Description:Technologies of the future could drive two companies
-              into the most exclusive club in the stock market.
-            </h4>
-          </div>
-          <div className="news_date">
-            <h4>Date:</h4>
-          </div>
-        </div>
+      <div className="news_container flex justify-center">
+        {news?.map((news_data) => {
+          return (
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href={news_data?.article_url}
+              className="newscard w-[17rem]  bg-white border border-gray-200 rounded-lg shadow m-[1rem] flex flex-col justify-between"
+            >
+              <div className="news_img">
+                <img
+                  src={news_data?.image_url}
+                  alt=""
+                  className="w-[100%] h-[10rem]"
+                />
+              </div>
+              <div className="news_details p-[0.5rem]">
+                <div className="news_title">{news_data?.title}</div>
+                
+              </div>
+            </a>
+          );
+        })}
       </div>
     </div>
   );
